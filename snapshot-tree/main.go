8eb7c2b92984c53578ef01d3cd2e126cb438fc72
Oliver, Jonathan <git@jonathanoliver.com>
2014-09-16 22:43:30 -0600

    TODOs.

--- snapshot-tree/main.go
+++ snapshot-tree/main.go
@@ -8,6 +8,11 @@ import (
 func main() {
 	workingDirectory, _ := os.Getwd()
 
+	// TODO: flags from command line:
+	// --makefile (generate a "forwarding makefile")
+	// --dryrun (indicates if we're not actually copying the files)
+	// --output=dir (the location of the directory in which to place the files)
+
 	files := BuildTree(workingDirectory)
 	dryRun := false             // TODO: from command line
 	outputDirectory := "output" // TODO: complain if the target exists? (or write to a temp dir?)
